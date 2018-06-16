c5571e2c1eb0da5a4e478a36c79f44554230b66d
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-06-16 16:32:05 +0530

    Medi Api

---
+++ serialize/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const db = require('./models');
+const areaRoutes = require('./routes/areaRoutes.js');
+const pharmaRoutes = require('./routes/pharmaRoutes.js');
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+//static directory
+app.use(express.static("app/public"));
+
+areaRoutes(app, db);
+pharmaRoutes(app, db);
+
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log(`listening on PORT ${PORT}`);
+    });
+});
+
