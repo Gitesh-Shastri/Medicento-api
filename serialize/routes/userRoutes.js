18c6234b19a5518bfa91ca3f6e69af86ca4bd271
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-06-20 13:09:23 +0530

    sq

--- serialize/routes/userRoutes.js
+++ serialize/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const express= require('express');
 const router = express.Router();
 const User = require('../models/user');
