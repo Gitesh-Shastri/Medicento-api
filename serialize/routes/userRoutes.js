0bde12810b3f750b1700413e0e4f89adada4d61a
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-06-20 13:01:26 +0530

    dd

--- serialize/routes/userRoutes.js
+++ serialize/routes/userRoutes.js
@@ -1,8 +1,8 @@
-const User = require('../models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const express= require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
         User.find()
