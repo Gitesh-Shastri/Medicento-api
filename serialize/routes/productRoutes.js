732048f2d3d38e63cce0fe76c6bf9d6331e58a44
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-07-08 20:44:07 +0530

    Update productRoutes.js

--- serialize/routes/productRoutes.js
+++ serialize/routes/productRoutes.js
@@ -102,7 +102,7 @@ router.post('/order', (req, res, next) => {
     order.grand_total = total;
     order.status = 'active';
     for(i=0;i< count;i++){
-        order.orders.push(orders[i]);    
+        order.order_items.push(orders[i]);    
     }
     order.save();
     Person.findOne({ _id:req.body[0].salesperson_id })
