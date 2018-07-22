c30eff2ee1c0e4bc7a5c44c5afeac34a292c808b
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-07-22 10:03:47 +0530

    Update productRoutes.js

--- serialize/routes/productRoutes.js
+++ serialize/routes/productRoutes.js
@@ -11,8 +11,46 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/medi',(req, res) => {
+    const inventoryProduct = InventoryProduct({
+        inventory_product_id: new mongoose.Types.ObjectId(),
+        inventory_id: req.body.inventory_id,
+        product_name: req.body.product_name,
+        stock_left: req.body.stock_left,
+        discount: req.body.discount,
+        cost_for_medicento: req.body.cost_for_medicento,
+        manufacturing_date: req.body.manufacturing_date,
+        expiry_date: req.body.expiry_date,
+        price_to_seller: req.body.price_to_seller,
+        price_to_retailer: req.body.cost_for_medicento,
+        tax_price_to_retailer: req.body.tax_price_to_retailer,
+        tax_percentage: req.body.tax_percentage,
+        scheme: req.body.scheme,
+        percentage_scheme: req.body.percentage_scheme,
+        batch_number: req.body.batch_number
+    });
+    inventoryProduct.save();
+    const product = Product({
+        product_id: new mongoose.Types.ObjectId(),
+        medicento_name: req.body.medicento_name,
+        product_code: req.body.product_code,
+        company_name: req.body.company_name,
+        total_stock: req.body.total_stock,
+        contents: req.body.contents,
+        package_type: req.body.package_type,
+        description: req.body.description,
+        category: req.body.category,
+        box_quantity: req.body.box_quantity
+    });
+    product.save();
+    const productandmedi = new ProductAndMedi({
+        product_id: product._id,
+        inventory_product_id: inventoryProduct._id 
+    });
+    productandmedi.save();
     res.status(200).json({
-        result: req.body.length
+        ipr: inventoryProduct,
+        pr: product,
+        me: productandmedi
     });
 });
 
