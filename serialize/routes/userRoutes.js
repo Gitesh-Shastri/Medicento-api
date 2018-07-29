873aab11efbbf8582c5cf464dbcc9b01c79ec90e
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-07-29 17:45:07 +0000

    Update userRoutes.js

--- serialize/routes/userRoutes.js
+++ serialize/routes/userRoutes.js
@@ -59,7 +59,7 @@ router.post('/salesPerson', (req, res, next) => {
             }) });
 
 router.get('/login', (req, res, next) => {
-    User.findOne({ usercode: req.query.usercode})
+    User.findOne({ userrmail: req.query.useremail, password: req.body.password})
         .exec()
         .then(user => {
             console.log(user);
