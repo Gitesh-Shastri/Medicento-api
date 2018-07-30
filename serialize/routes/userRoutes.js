a572a9ab721648c1377c253b53d2f3b19b4102a0
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-07-30 15:19:46 +0530

    Update userRoutes.js

--- serialize/routes/userRoutes.js
+++ serialize/routes/userRoutes.js
@@ -59,7 +59,8 @@ router.post('/salesPerson', (req, res, next) => {
             }) });
 
 router.get('/login', (req, res, next) => {
-    User.findOne({ useremail: req.query.useremail, password: req.body.password })
+    if(req.query.useremail != null) {
+        User.findOne({ useremail: req.query.useremail, password: req.body.password })
         
         .exec()
         .then(user => {
@@ -77,6 +78,26 @@ router.get('/login', (req, res, next) => {
                 message: "Invalid Useremail or password"
             });
         });
+    } else {
+        User.findOne({ useremail: req.query.useremail, password: req.body.password })
+        
+        .exec()
+        .then(user => {
+            console.log(user);
+            Person.find({user: user._id})
+                  .exec()
+                  .then(doc => {
+                    res.status(200).json({
+                        Sales_Person: doc
+                    })  
+                  });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "Invalid Useremail or password"
+            });
+        });    
+    }
 });
 
 router.post('/login', (req, res, next) => {
