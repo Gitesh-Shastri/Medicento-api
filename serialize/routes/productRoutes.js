961808fafd7f8f8aebd99789e4bd7ba58c547864
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-08-01 21:03:06 +0530

    Update productRoutes.js

--- serialize/routes/productRoutes.js
+++ serialize/routes/productRoutes.js
@@ -44,7 +44,7 @@ router.get('/m', (req, res) => {
             user: "giteshshastri123@outlook.com",
             pass: "shastri@1"
         }, from: 'giteshshastri123@outlook.com',
-        to: 'giteshshastri96@gmail.com, giteshshastri96@gmail.com ',
+        to: 'giteshshastri96@gmail.com, giteshshastri100@gmail.com ',
         subject: 'Heroku Test',
         html: '<b>This is bold text</b>',
     });
