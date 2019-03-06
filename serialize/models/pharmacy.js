0af7f6b2e1e88d89f286843867ae9e1ff428d37c
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2019-03-06 20:05:55 +0530

    Update pharmacy.js

--- serialize/models/pharmacy.js
+++ serialize/models/pharmacy.js
@@ -7,28 +7,48 @@ const pharmaSchema = mongoose.Schema({
 		ref: 'Area'
 	},
 	pharma_name: {
-		type: String
+		type: String,
+		default: "-"
 	},
 	pharma_address: {
-		type: String
+		type: String,
+		default: "-"
 	},
 	gst_license: {
-		type: String
+		type: String,
+		default: "-"
 	},
 	drug_license: {
-		type: String
+		type: String,
+		default: "-"
 	},
 	email: {
-		type: String
+		type: String,
+		default: "-"
 	},
 	contact: {
-		type: String
+		type: String,
+		default: "-"
 	},
 	owner_name: {
-		type: String
+		type: String,
+		default: "-"
 	},
 	pincode: {
-		type: String	
+		type: String,
+		default: "-"	
+	},
+	pan_card: {
+		type: String,
+		default: "-"	
+	},
+	distributor: {
+		type: String,
+		default: "-"
+	},
+	distributor_Code: {
+		type: String,
+		default: "-"
 	}
 });
 
