b0fb1e3eed7669d7f582e0f832b36f2aaeaff979
apple <giteshshastri96@gmail.com>
2019-06-12 00:17:28 +0530

    Changes For Topic And User

--- serialize/routes/productRoutes.js
+++ serialize/routes/productRoutes.js
@@ -666,101 +666,103 @@ router.post('/order', (req, res, next) => {
 	var csv = 'Party_Code, Item_Code, Item_Name, Qty\n';
 	var date = new Date();
 	console.log(date.toISOString());
-	OrderCode.find().exec().then((doc_order_code) => {
-		Pharmacy.findOne({
-			_id: req.body[0].pharma_id
-		})
-			.populate('area')
-			.exec()
-			.then((docp) => {
-				message =
-					'<h3>Pharmacy Name: ' +
-					docp.pharma_name +
-					'</h4><h4>Address : ' +
-					docp.pharma_address +
-					'</h4>' +
-					'<h4>Chosen Slot : ' +
-					req.body[0].slot +
-					'</h4>';
-				message +=
-					'<table border="1" style="width:100%"><tr><th style="width:60%">Item_Name</th><th style="width:20%">Item_Code</th><th style="width:10%">Quantity</th><th style="width:10%">Cost</th></tr>';
-				var deliverdate = date;
-				deliverdate.setDate(deliverdate.getDate() + 1);
-				deliverdate = deliverdate.toLocaleDateString();
-				count = req.body.length - 1;
-				total = 0;
-				orderid = '';
-				for (i = 0; i < count; i++) {
-					cost = Number(req.body[i].cost);
-					total += cost;
-				}
-				Person.findOne({
-					_id: req.body[0].salesperson_id
-				})
-					.populate('user')
-					.exec()
-					.then((salesP) => {
-						orders = [];
-						for (i = 0; i < count; i++) {
-							const orderItem = new OrderItem();
-							(orderItem.quantity = req.body[i].qty),
-								(orderItem.paid_price = req.body[i].rate),
-								(orderItem.code = req.body[i].code),
-								(orderItem.created_at = date),
-								(orderItem.medicento_name = req.body[i].medicento_name),
-								(orderItem.company_name = req.body[i].company_name),
-								(orderItem.total_amount = req.body[i].cost);
-							orderItem.save();
-							orders.push(orderItem._id);
-							csv +=
-								docp.distributor_Code +
-								',' +
-								req.body[i].code +
-								',' +
-								req.body[i].medicento_name +
-								',' +
-								req.body[i].qty +
-								'\n';
-							message +=
-								'<tr><td style="width:60%">' +
-								req.body[i].medicento_name +
-								'</td><td style="width:20%">' +
-								req.body[i].code +
-								'</td><td style="width:10%">' +
-								req.body[i].qty +
-								'</td><td style="width:10%">' +
-								req.body[i].cost +
-								'</td></tr>';
-						}
-						console.log(doc_order_code[0].code);
-						const order = new Order();
-						order.created_at = date;
-						order.order_slot = req.body[0].slot;
-						order.pharmacy_id = req.body[0].pharma_id;
-						order.sales_person_id = req.body[0].salesperson_id;
-						order.sales_order_code = doc_order_code[0].code;
-						order.grand_total = total;
-						order.delivery_date = deliverdate;
-						order.status = 'Active';
-						for (i = 0; i < count; i++) {
-							order.order_items.push(orders[i]);
-						}
-						order.save();
-						console.log(order);
-						OrderCode.findOneAndUpdate(
-							{
-								_id: doc_order_code[0]._id
-							},
-							{
-								$set: {
-									code: doc_order_code[0].code + 1
-								}
-							},
-							{
-								new: true
-							},
-							(error, update_code) => {
-								/*  var message1	= {
+	OrderCode.find()
+		.exec()
+		.then((doc_order_code) => {
+			Pharmacy.findOne({
+				_id: req.body[0].pharma_id
+			})
+				.populate('area')
+				.exec()
+				.then((docp) => {
+					message =
+						'<h3>Pharmacy Name: ' +
+						docp.pharma_name +
+						'</h4><h4>Address : ' +
+						docp.pharma_address +
+						'</h4>' +
+						'<h4>Chosen Slot : ' +
+						req.body[0].slot +
+						'</h4>';
+					message +=
+						'<table border="1" style="width:100%"><tr><th style="width:60%">Item_Name</th><th style="width:20%">Item_Code</th><th style="width:10%">Quantity</th><th style="width:10%">Cost</th></tr>';
+					var deliverdate = date;
+					deliverdate.setDate(deliverdate.getDate() + 1);
+					deliverdate = deliverdate.toLocaleDateString();
+					count = req.body.length - 1;
+					total = 0;
+					orderid = '';
+					for (i = 0; i < count; i++) {
+						cost = Number(req.body[i].cost);
+						total += cost;
+					}
+					Person.findOne({
+						_id: req.body[0].salesperson_id
+					})
+						.populate()
+						.exec()
+						.then((salesP) => {
+							orders = [];
+							for (i = 0; i < count; i++) {
+								const orderItem = new OrderItem();
+								(orderItem.quantity = req.body[i].qty),
+									(orderItem.paid_price = req.body[i].rate),
+									(orderItem.code = req.body[i].code),
+									(orderItem.created_at = date),
+									(orderItem.medicento_name = req.body[i].medicento_name),
+									(orderItem.company_name = req.body[i].company_name),
+									(orderItem.total_amount = req.body[i].cost);
+								orderItem.save();
+								orders.push(orderItem._id);
+								csv +=
+									docp.distributor_Code +
+									',' +
+									req.body[i].code +
+									',' +
+									req.body[i].medicento_name +
+									',' +
+									req.body[i].qty +
+									'\n';
+								message +=
+									'<tr><td style="width:60%">' +
+									req.body[i].medicento_name +
+									'</td><td style="width:20%">' +
+									req.body[i].code +
+									'</td><td style="width:10%">' +
+									req.body[i].qty +
+									'</td><td style="width:10%">' +
+									req.body[i].cost +
+									'</td></tr>';
+							}
+							console.log(doc_order_code[0].code);
+							const order = new Order();
+							order.created_at = date;
+							order.order_slot = req.body[0].slot;
+							order.pharmacy_id = req.body[0].pharma_id;
+							order.sales_person_id = req.body[0].salesperson_id;
+							order.sales_order_code = doc_order_code[0].code;
+							order.grand_total = total;
+							order.delivery_date = deliverdate;
+							order.status = 'Active';
+							for (i = 0; i < count; i++) {
+								order.order_items.push(orders[i]);
+							}
+							order.save();
+							console.log(order);
+							OrderCode.findOneAndUpdate(
+								{
+									_id: doc_order_code[0]._id
+								},
+								{
+									$set: {
+										code: doc_order_code[0].code + 1
+									}
+								},
+								{
+									new: true
+								},
+								(error, update_code) => {
+									/*  var message1	= {
                                          text:	"i hope this works", 
                                          from:	"Gitesh <giteshmedicento@gmail.com>", 
                                          to:		"Gitesh <giteshmedicento@gmail.com>",
@@ -774,86 +776,87 @@ router.post('/order', (req, res, next) => {
                                       // send the message and get a callback with an error or details of the message that was sent
                                       server.send(message1, function(err, message) { console.log(err || message); });
                                    */
-								/*var nodemailer = require('nodemailer');
+									/*var nodemailer = require('nodemailer');
  /*   const mailOptions = {
 	    from: 'giteshmedicento@gmail.com', // sender address
 	    to: 'giteshshastri100@gmail.com', // list of receivers
 	    html: message + '<p>Grand Total = ' + total +'</p>'// plain text body
     };
     */
-								console.log(csv);
-								Person.findOne({
-									_id: req.body[0].salesperson_id
-								})
-									.populate('user')
-									.exec()
-									.then((sales) => {
-										content =
-											'Order has been placed by ' +
-											docp.pharma_name +
-											' on ' +
-											date.toISOString(); // Subject line
-										message =
-											message +
-											'<td style="width:60%"></td><td colspan="2" style="width:40%">Grand Total = ' +
-											total +
-											'</td>';
-										nodeoutlook.sendEmail({
-											auth: {
-												user: 'Team.medicento@outlook.com',
-												pass: 'med4lyf@51'
-											},
-											from: 'Team.medicento@outlook.com',
-											to: 'giteshshastri96@gmail.com,contact.medicento@gmail.com',
-											subject: content,
-											html: message,
-											attachments: [
+									console.log(csv);
+									Person.findOne({
+										_id: req.body[0].salesperson_id
+									})
+										.populate()
+										.exec()
+										.then((sales) => {
+											content =
+												'Order has been placed by ' +
+												docp.pharma_name +
+												' on ' +
+												date.toISOString(); // Subject line
+											message =
+												message +
+												'<td style="width:60%"></td><td colspan="2" style="width:40%">Grand Total = ' +
+												total +
+												'</td>';
+											nodeoutlook.sendEmail({
+												auth: {
+													user: 'Team.medicento@outlook.com',
+													pass: 'med4lyf@51'
+												},
+												from: 'Team.medicento@outlook.com',
+												to: 'giteshshastri96@gmail.com,contact.medicento@gmail.com',
+												subject: content,
+												html: message,
+												attachments: [
+													{
+														filename:
+															'SalesOrder_Medicento_' +
+															docp.pharma_name +
+															'_' +
+															date.toISOString() +
+															'.csv',
+														content: csv
+													}
+												]
+											});
+											console.log('Sales id : ', sales);
+											Person.update(
 												{
-													filename:
-														'SalesOrder_Medicento_' +
-														docp.pharma_name +
-														'_' +
-														sales.user.useremail +
-														'_' +
-														date.toISOString() +
-														'.csv',
-													content: csv
+													_id: sales._id
+												},
+												{
+													Total_sales: sales.Total_sales + total,
+													No_of_order: sales.No_of_order + 1,
+													Earnings: sales.commission * (sales.Total_sales + total)
 												}
-											]
-										});
-										console.log('Sales id : ', sales);
-										Person.update(
-											{
-												_id: sales._id
-											},
-											{
-												Total_sales: sales.Total_sales + total,
-												No_of_order: sales.No_of_order + 1,
-												Earnings: sales.commission * (sales.Total_sales + total)
-											}
-										)
-											.exec()
-											.then((err, updated) => {
-												/*             transporter.sendMail(mailOptions, function (err, info) {
+											)
+												.exec()
+												.then((err, updated) => {
+													/*             transporter.sendMail(mailOptions, function (err, info) {
                         if(err)
                           console.log(err)
                         else
                           console.log(info);
                      });
 		*/
-												res.status(200).json({
-													message: 'Order has been placed successfully',
-													delivery_date: order.delivery_date.toLocaleString(),
-													order_id: '' + order.sales_order_code,
-													grand_total: order.grand_total
+													res.status(200).json({
+														message: 'Order has been placed successfully',
+														delivery_date: order.delivery_date.toLocaleString(),
+														order_id: '' + order.sales_order_code,
+														grand_total: order.grand_total
+													});
 												});
-											});
-									});
-							}
-						);
-					});
-			});
-	});
+										});
+								}
+							);
+						});
+				});
+		})
+		.catch((err) => {
+			res.status(200).json({ error: err });
+		});
 });
 
 router.get('/csv', (req, res, next) => {
